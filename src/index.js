b7b5abcd3a6a8ea2e1c1f11240dfaeaba7688afc
Bodor István <istvanbodor19&gmail.com>
2022-11-09 13:19:08 +0100

    regexek

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let lista;
+document.addEventListener('DOMContentLoaded', () => {
+    var _a;
+    (_a = document.getElementById('gomb')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        var _a, _b, _c, _d;
+        document.getElementById('nevhiba').textContent = "";
+        document.getElementById('evhiba').textContent = "";
+        document.getElementById('arhiba').textContent = "";
+        document.getElementById('magassaghiba').textContent = "";
+        let nevellenorzo = /^[A-Za-z','' ']+$/;
+        let nev = (_a = document.getElementById('nev')) === null || _a === void 0 ? void 0 : _a.value;
+        let evellenorzo = /^([1-9]|[0-9][0-9][0-9]|2022)$/;
+        let ev = (_b = document.getElementById('ev')) === null || _b === void 0 ? void 0 : _b.value;
+        let arellenorzo = /^[1-9]{1}[0-9]*$/;
+        let ar = parseInt((_c = document.getElementById('ar')) === null || _c === void 0 ? void 0 : _c.value);
+        let magassagellenorzo = /^[1-9]{1}[0-9]+$/;
+        let magassag = parseInt((_d = document.getElementById('magassag')) === null || _d === void 0 ? void 0 : _d.value);
+        if (!nevellenorzo.test(nev)) {
+            document.getElementById('nevhiba').textContent = "A név mező helytelen";
+        }
+        else if (!evellenorzo.test(ev)) {
+            document.getElementById('evhiba').textContent = "Az év mező helytelen";
+        }
+        else if (!arellenorzo.test(ar.toString())) {
+            document.getElementById('arhiba').textContent = "Az ár mező helytelen";
+        }
+        else if (!magassagellenorzo.test(magassag.toString())) {
+            document.getElementById('magassaghiba').textContent = "A magasság mező helytelen";
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,86 @@
 import { Statue } from "./statue";
+import { Artwork } from "./artwork";
+
+let lista : Artwork[];
 
 
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+      document.getElementById('gomb')?.addEventListener('click', () =>{
+
+        (document.getElementById('nevhiba') as HTMLLabelElement).textContent = "";
+        (document.getElementById('evhiba') as HTMLLabelElement).textContent = "";
+        (document.getElementById('arhiba') as HTMLLabelElement).textContent = "";
+        (document.getElementById('magassaghiba') as HTMLLabelElement).textContent = "";
+
+
+
+
+
+        let nevellenorzo =  /^[A-Za-z','' ']+$/ ;
+        let nev : string= (document.getElementById('nev') as HTMLInputElement)?.value;
+
+        let evellenorzo = /^([1-9]|[0-9][0-9][0-9]|2022)$/;
+        let ev : string= (document.getElementById('ev') as HTMLInputElement)?.value;
+
+        let arellenorzo = /^[1-9]{1}[0-9]*$/;
+        let ar : number = parseInt((document.getElementById('ar') as HTMLInputElement)?.value);
+
+        let magassagellenorzo = /^[1-9]{1}[0-9]+$/;
+        let magassag : number = parseInt((document.getElementById('magassag') as HTMLInputElement)?.value);
+
+
+
+        if(!nevellenorzo.test(nev))
+        {
+
+            (document.getElementById('nevhiba') as HTMLLabelElement).textContent = "A név mező helytelen"
+
+        }
+        else if (!evellenorzo.test(ev))
+        {
+
+            (document.getElementById('evhiba') as HTMLLabelElement).textContent = "Az év mező helytelen"
+
+        }
+        else if (!arellenorzo.test(ar.toString()))
+        {
+
+            (document.getElementById('arhiba') as HTMLLabelElement).textContent = "Az ár mező helytelen"
+
+        }
+        else if (!magassagellenorzo.test(magassag.toString()))
+        {
+
+            (document.getElementById('magassaghiba') as HTMLLabelElement).textContent = "A magasság mező helytelen"
+
+
+        }
+
+
+
+
+
+
+
+
+
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
